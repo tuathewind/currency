18317fec29c767192628e52b84be4e508b6ed968
tuathewind <kiseleva7811@gmail.com>
2024-09-03 17:09:39 +0400

    git

---
+++ __init__.py
@@ -0,0 +1,37 @@
+from datetime import datetime
+
+from airflow import DAG
+from airflow.providers.postgres.operators.postgres import PostgresOperator
+
+from operator import CurrencyScoopOperator
+
+with DAG(
+    dag_id='exchange_rate_usd_kzt_dag',
+    start_date=datetime(2023, 1, 1),
+    schedule_interval='@daily'
+) as dag:
+    create_table=PostgresOperator(
+        task_id='create_table_task',
+        sql='Users/tuathewind/PycharmProjects/currency/sql/create_table.sql',
+        postgres_conn_id='postgres_default'
+    )
+
+    get_rate=CurrencyScoopOperator(
+        task_id='get_rate',
+        base_currency='USD',
+        currency='KZT',
+        conn_id='cur_scoop_conn_id',
+        dag=dag,
+    )
+
+    insert_rate=PostgresOperator(
+        task_id='insert_rate',
+        postgres_conn_id='postgres_default',
+        sql='Users/tuathewind/PycharmProjects/currency/sql/insert_rate.sql',
+        params={
+            'base_currency': 'USD',
+            'currency': 'KZT',
+        }
+    )
+
+    create_table >> get_rate >> insert_rate
